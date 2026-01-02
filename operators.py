2d0c1814d876922552c4339bb375f2b481ee3747
ariyawahidstudentse-ship-it <ariyawahid.student.se@gmail.com>
2026-01-02 16:50:13 +0600

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,13 @@
+n1 = 49
+n2 = 67
+
+print("number1 :", n1)
+print("number2 :", n2)
+print("Addition :", n1 + n2)
+print("Subtraction :", n2 - n1)
+print("Multiplication :", n1 * n2)
+print("Division :", n2 / n1)
+print("Floor Division :", n2 // n1)
+print("Modulus :", n2 % n1)
+print("square :", n2**2)
+print("square root :", n1**0.5)
