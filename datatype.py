5eee36b6fc8e7fc199e1d8f567ea796e05300b43
ariyawahidstudentse-ship-it <ariyawahid.student.se@gmail.com>
2026-01-02 16:41:12 +0600

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,23 @@
+name = "Ariya"
+age = 11
+is_student = True
+weight = 35.5
+
+print("name:", name)
+print("The type of name is", type(name))
+print("age:", age)
+print("The type of age is", type(age))
+print("is_student:", is_student)
+print("The type of is_student is", type(is_student))
+print("weight:", weight)
+print("The type of weight is", type(weight))
+
+
+print("\n After type casting. . .")
+age = str(age)
+print(age)
+print("the type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("the type of weight is", type(weight))
+
