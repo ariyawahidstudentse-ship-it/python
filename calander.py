0745f8da4b2a94ce8c217e7e7329d510c9e9740c
ariyawahidstudentse-ship-it <ariyawahid.student.se@gmail.com>
2026-01-03 16:54:47 +0600

    Add files via upload

---
+++ calander.py
@@ -0,0 +1,8 @@
+import datetime
+current_time = datetime.datetime.now()
+print("Current date and time is:", current_time)
+print(current_time)
+
+import calendar
+print("\n", calendar.calendar(2026))
+
