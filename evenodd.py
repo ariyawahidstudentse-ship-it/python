2fa4fa1b70f1aacf8670b68c4ef827f55ebfd979
ariyawahidstudentse-ship-it <ariyawahid.student.se@gmail.com>
2026-01-03 16:26:24 +0600

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number to check"))
+print("Number to be checked :", number)
+
+if number%2==0:
+    print(number, "is an Even number")
+else:
+    print(number, "is an Odd number")
+    
